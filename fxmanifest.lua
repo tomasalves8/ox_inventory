5e50e027493d42b29ddc571a28628f6bdfaf6f2b
klandestino7 <klandestino6@hotmail.com>
2024-05-15 18:23:22 -0300

    fix(inv/fxmanifest): listing new assets

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -41,6 +41,8 @@ files {
     'web/build/index.html',
     'web/build/assets/*.js',
     'web/build/assets/*.css',
+    'web/build/assets/*.png',
+    'web/build/assets/*.ttf',
     'web/images/*.png',
     'modules/**/shared.lua',
     'modules/**/client.lua',
