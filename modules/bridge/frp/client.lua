1103862892c5fdd53582cb094dd1488e8ea2f8b8
klandestino7 <klandestino6@hotmail.com>
2024-05-18 18:51:05 -0300

    tweak: change logout event name

--- modules/bridge/frp/client.lua
+++ modules/bridge/frp/client.lua
@@ -5,7 +5,7 @@ local Weapon = require 'modules.weapon.client'
 
 Tunnel.bindInterface("inventory", Inventory)
 
-RegisterNetEvent('API:UserLogout', client.onLogout)
+RegisterNetEvent('API:onSessionStoppedPlaying', client.onLogout)
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function client.setPlayerStatus(values)
