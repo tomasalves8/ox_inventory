7c9ebf24cb0e9c5f68a6b265ebf83e6b6c952fd7
Tomás Alves <tomasalves35@gmail.com>
2024-10-04 21:27:58 +0100

    Remove FRP Lib

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
-games { 'rdr3', 'gta5' }
+games {'rdr3', 'gta5'}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 name 'ox_inventory'
 author 'Overextended'
@@ -9,44 +9,19 @@ version '2.39.1'
 repository 'https://github.com/overextended/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
-dependencies {
-    '/server:6116',
-    '/onesync',
-    'oxmysql',
-    'ox_lib',
-}
+dependencies {'/server:6116', '/onesync', 'oxmysql', 'ox_lib'}
 
 shared_script '@ox_lib/init.lua'
-shared_script '@frp_lib/library/linker.lua'
 
-ox_libs {
-    'locale',
-    'table',
-    'math',
-}
+ox_libs {'locale', 'table', 'math'}
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'init.lua'
-}
+server_scripts {'@oxmysql/lib/MySQL.lua', 'init.lua'}
 
 client_script 'init.lua'
 
 ui_page 'web/build/index.html'
 
-files {
-    'client.lua',
-    'server.lua',
-    'locales/*.json',
-    'web/build/index.html',
-    'web/build/assets/*.js',
-    'web/build/assets/*.css',
-    'web/build/assets/*.png',
-    'web/build/assets/*.ttf',
-    'web/images/*.png',
-    'modules/**/shared.lua',
-    'modules/**/client.lua',
-    'modules/bridge/**/client.lua',
-    'data/*.lua',
-    'data/items/*.lua',
-}
+files {'client.lua', 'server.lua', 'locales/*.json', 'web/build/index.html', 'web/build/assets/*.js',
+       'web/build/assets/*.css', 'web/build/assets/*.png', 'web/build/assets/*.ttf', 'web/images/*.png',
+       'modules/**/shared.lua', 'modules/**/client.lua', 'modules/bridge/**/client.lua', 'data/*.lua',
+       'data/items/*.lua'}
